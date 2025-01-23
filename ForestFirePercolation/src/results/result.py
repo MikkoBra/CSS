2e079690d1e44875854e8bd7c83b6831f0b39a17
MikkoBra <m.brandon.1@student.rug.nl>
2025-01-23 14:50:00 +0100

    Result class matched to csv output

--- ForestFirePercolation/src/results/result.py
+++ ForestFirePercolation/src/results/result.py
@@ -1,9 +1,8 @@
 class Result:
-    def __init__(self, density, system_size, wind, env_index, percent_burned, percolated, percent_percolated):
+    def __init__(self, size, density, wind, env_index, percolation, percentage_burnt_down):
         self.density = density
-        self.system_size = system_size
+        self.size = size
         self.wind = wind
         self.env_index = env_index
-        self.percent_burned = percent_burned
-        self.percolated = percolated
-        self.percent_percolated = percent_percolated
+        self.percolation = percolation
+        self.percentage_burnt_down = percentage_burnt_down
