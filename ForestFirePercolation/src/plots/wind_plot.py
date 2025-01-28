7dddc9ea7c81bfa461ff36184bf61275fbca41a2
MikkoBra <m.brandon.1@student.rug.nl>
2025-01-28 15:05:11 +0100

    Started wind plots

--- ForestFirePercolation/src/plots/wind_plot.py
+++ ForestFirePercolation/src/plots/wind_plot.py
@@ -4,24 +4,23 @@ from percolation_plot import PercolationPlot
 
 class WindPlot(PercolationPlot):
 
-    def plot_wind_vs_no_wind(self):
+    def plot_wind_vs_no_wind(self, results_per_wind_setting, title):
         """
             Function that creates a plot of percolation probability vs forest density for all sets of saved results in
             the same figure.
         """
         fig, ax = plt.subplots()
-        result_idx = 0
-        results = self.get_result(result_idx)
-        label = r'Wind = True'
-        self.plot_percolation_vs_density(results.densities, results.probabilities, results.system_size, ax, label)
-
-        result_idx += 1
-        label = r'Wind = False'
-        results = self.get_result(result_idx)
-        self.plot_percolation_vs_density(results.densities, results.probabilities, results.system_size, ax, label)
-
+        for wind_setting_results in results_per_wind_setting:
+            for system_size in wind_setting_results:
+                self.save_amount_percolated_per_density(wind_setting_results[system_size])
+                self.system_size = system_size + results_per_wind_setting['label_suffix']
+                self.plot_single_percolation_vs_density(ax)
+            # Add critical point and/or zoom in on critical point
+            critical_point = results_per_wind_setting['crit']
+            if critical_point != 0.0:
+                ax.axvline(x=critical_point, ls='--', label=r'$d_c$ = ' + str(critical_point))
         ax.set_xlabel(r'Density $d$')
         ax.set_ylabel(r'$P_N$')
         ax.legend()
-        ax.set_title('Percolation probability vs forest density for wind and no wind')
+        ax.set_title(title)
         plt.show()
