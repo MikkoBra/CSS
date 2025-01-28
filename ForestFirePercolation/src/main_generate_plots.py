57cfa7175a9aa00e9e96e8f456a221c7e0448305
MikkoBra <m.brandon.1@student.rug.nl>
2025-01-28 19:03:29 +0100

    SOC plots set up

--- ForestFirePercolation/src/main_generate_plots.py
+++ ForestFirePercolation/src/main_generate_plots.py
@@ -2,10 +2,11 @@ from ForestFirePercolation.src.plots.plot_generator import PlotGenerator
 
 if __name__ == "__main__":
     generator = PlotGenerator("Snellius/totaldensity_extended_extra.csv")
-    # generator.generate_base_experiment_plots()
-    # generator.generate_wind_experiment_plots()
-    # generator.generate_env_index_experiment_plots()
-    # generator.generate_env_index_050_and_wind_experiment_plots()
-    # generator.generate_plant_experiment_plots()
+    generator.generate_base_experiment_plots()
+    generator.generate_wind_experiment_plots()
+    generator.generate_env_index_experiment_plots()
+    generator.generate_env_index_050_and_wind_experiment_plots()
+    generator.generate_plant_experiment_plots()
     generator.generate_base_SOC_plots()
     generator.generate_env_index_SOC_plots()
+    # Other SOC plots are excluded due to lack of data
